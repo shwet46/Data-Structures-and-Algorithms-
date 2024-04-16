41a5057d39e0a18b038e29faea0b3bef86f930ac
Shweta Behera <143203479+shwet46@users.noreply.github.com>
2024-04-17 00:30:45 +0530

    Add files via upload

---
+++ validparenthesis.java
@@ -0,0 +1,46 @@
+import java.util.Stack;
+
+public class validparenthesis {
+
+    public static boolean isValid(String str) { // time complexity -> O(n)
+        Stack<Character> s = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                s.push(ch);
+            } else {
+                // closing
+                if (s.isEmpty()) {
+                    return false;
+                }
+
+                if ((s.peek() == '(' && ch == ')') // ()
+                        || (s.peek() == '{' && ch == '}') // {}
+                        || (s.peek() == '[' && ch == ']')) { // []
+                    s.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        if (s.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        String par = "({[]}())";
+        System.out.println(isValid(par));
+
+        String par1 = "({]}({))";
+        System.out.println(isValid(par1));
+
+    }
+
+}
