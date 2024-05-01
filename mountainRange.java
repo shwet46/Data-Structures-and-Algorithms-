6da0a7e9f1bf8d7102e5f3e0d9ed16e3d189279f
Shweta Behera <143203479+shwet46@users.noreply.github.com>
2024-05-01 19:31:54 +0530

    Add files via upload

---
+++ mountainRange.java
@@ -0,0 +1,28 @@
+public class mountainRange {
+
+    public static int mountRanges(int n) {
+        int dp[] = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i < n + 1; i++) {
+            for (int j = 0; j < i; j++) {
+                int inside = dp[j];
+                int outside = dp[i - j - 1];
+                dp[i] += inside * outside;
+                // Ci = Cj * C(i-j-1)
+            }
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        System.out.println(mountRanges(n));
+
+    }
+
+}
